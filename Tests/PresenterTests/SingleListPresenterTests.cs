900d277747445714f1fbc68fd736ff42982921ef
David Adsit <david.adsit@gmail.com>
2011-09-11 11:46:26 -0600

    With the dependencies loosened, adding a test for HandleSaveChanges was easy.

--- Tests/PresenterTests/SingleListPresenterTests.cs
+++ Tests/PresenterTests/SingleListPresenterTests.cs
@@ -1,4 +1,8 @@
-﻿using NUnit.Framework;
+﻿using Business;
+using Moq;
+using NUnit.Framework;
+using WebSite.Logic.Presenters;
+using WebSite.Logic.Views;
 
 namespace Tests.PresenterTests
 {
@@ -8,7 +12,16 @@ namespace Tests.PresenterTests
 		[Test]
 		public void HandleSaveChanges_delegates_to_ListManager()
 		{
-			
+			//Arrange
+			Mock<ISingleListView> viewMock = new Mock<ISingleListView>();
+			Mock<IListManager> listManagerMock = new Mock<IListManager>();
+			SingleListPresenter presenter = new SingleListPresenter(viewMock.Object, listManagerMock.Object);
+
+			//Act
+			presenter.HandleSaveChanges();
+
+			//Assert
+			listManagerMock.Verify(x => x.SaveChanges(It.IsAny<int>(), It.IsAny<string>(), It.IsAny<string>()));
 		}
 	}
 }
